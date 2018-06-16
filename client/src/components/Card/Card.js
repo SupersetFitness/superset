fb8f7905cc144fb13c08434c8eb98cd32c63b96a
noorehtesham <noore44@vt.edu>
2018-06-16 12:41:42 -0400

    changes

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,1 +1,18 @@
 import react from 'react';
+
+const Card =(props) => {
+return (
+    <div>
+      <Card>
+        <CardBody>
+          <CardTitle>{props.trainerName}</CardTitle>
+          <CardSubtitle>{props.description}</CardSubtitle>
+          <CardText>{props.longerdescription}</CardText>
+          <Button>Select</Button>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default Card;
