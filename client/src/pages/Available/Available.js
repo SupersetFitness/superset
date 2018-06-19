b93fb87e8a390b85c36ea0478403ed6cd521b880
noorehtesham <noore44@vt.edu>
2018-06-19 10:31:48 -0400

    updates

--- client/src/pages/Available/Available.js
+++ client/src/pages/Available/Available.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Name from "../../components/Name";
 
 const Available = props => {
     <div>
       <h1>Hey, {props.username}! </h1>  
       <h3>Here are the available trainers in your area:</h3>
-      <Card/>
+      <Name/>
     </div>
 
 }
