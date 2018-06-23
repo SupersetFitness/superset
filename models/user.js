8ba5773fee34500db4dfa451640b6911acb2a93a
Nicolas Brent Ellerbe <nbrente@gmail.com>
2018-06-23 03:34:59 -0400

    80% passport integration.

--- models/user.js
+++ models/user.js
@@ -1,15 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
 
 const userSchema = new Schema({
   name: { 
     type: String,
     required: true 
-  },
-  // email: {
-  //   type: email,
-  //   required: true
-  // },
+  }, 
   address: { 
     type: String,
     required: true 
@@ -19,14 +17,29 @@ const userSchema = new Schema({
     required: false
   },
   focuses: { 
-    type: String,
+    type: Array,
     required: false
+  }, 
+  email: {
+    type: String,
+    required: true
+  }, 
+  password: {
+    // The password cannot be null and be within 6-15 characters in length
+    type: String,
+    notNull: true
+  },
+  isOnline: {
+    type: Boolean,
+    defaultValue: false
   },
   date: { 
     type: Date, 
     default: Date.now
   }
 });
+
+UserSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model("User", userSchema);
 
--- server.js
+++ server.js
@@ -1,9 +1,29 @@
+//new
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const passport = require("./config/passport");
+const LocalStrategy = require('passport-local').Strategy;
+//old
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const path = require("path");
+// Requiring passport as we've configured it
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+mongoose.Promise = global.Promise;
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+
+// passport configuration
+var User = require('./models/User');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//Defining routes
+const routes = require("./routes");
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,11 +32,23 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+// We need to use sessions to keep track of our user's login status
+app.use(session({
+  secret: "keyboard cat",
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+//error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 // Start the API server
 app.listen(PORT, function() {
