ef8ee25f307df80e068dda29ab535fde6f6b3bb1
Valerie Palomino <valeriepalomino@Valeries-MacBook-Pro.local>
2018-06-28 10:28:17 -0400

    issue fixed

--- client/src/components/chat/index.js
+++ client/src/components/chat/index.js
@@ -1,2 +1,2 @@
-export {defualt as Chat} from "./ChatSend.js"
-export {default as ChatPage} from "./ChatMessage.js"
+// export {defualt as Chat} from "./ChatSend.js"
+// export {default as ChatPage} from "./ChatMessage.js"
