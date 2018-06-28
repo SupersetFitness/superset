6f0c774eb71f339dd26e27e2b8398b5235e9bfa6
Valerie Palomino <valeriepalomino@Valeries-MacBook-Pro.local>
2018-06-27 22:50:51 -0400

    map update

--- client/src/components/Map/map.js
+++ client/src/components/Map/map.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import "./map.css";
  
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
  
@@ -15,16 +16,16 @@ class SimpleMap extends Component {
   render() {
     return (
       // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
+      <div style={{ height: '45vh', width: '45%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyDA18Xi0CP_7PZrX8NQd76lngn6VrhOgZQ' }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
           <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text={'Kreyser Avrora'}
+            lat={35.7796}
+            lng={78.6382}
+            text={'Raleigh, NC'}
           />
         </GoogleMapReact>
       </div>
